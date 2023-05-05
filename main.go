329553f277b0f9d8be249e68ef899235dde0d58f
Vivmoy <1352133298@qq.com>
2023-05-05 16:30:41 +0800

    完成了重加密密钥派生

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	message := "hello proxy-reencryption"
-	pub, pri, err1 := curve.KeyGen()
+	apub, apri, err1 := curve.KeyGen()
 	if err1 != nil {
-		fmt.Println("KeyGen() error:", err1.Error())
+		fmt.Println("aKeyGen() error:", err1.Error())
 	}
-	cipher, err2 := recrypt.Encrypt(pub, message)
+	cipher, err2 := recrypt.Encrypt(apub, message)
 	if err2 != nil {
 		fmt.Println("Encrypt() error:", err2.Error())
 	}
@@ -22,9 +22,18 @@ func main() {
 	} else {
 		fmt.Println("CheckCapsule() success")
 	}
-	plain, err4 := recrypt.Decrypt(pri, cipher)
+	plain, err4 := recrypt.Decrypt(apri, cipher)
 	if err4 != nil {
 		fmt.Println("Decrypt() error:", err4.Error())
 	}
+	bpub, _, err5 := curve.KeyGen()
+	if err5 != nil {
+		fmt.Println("bKeyGen() error:", err5.Error())
+	}
+	KF, err6 := recrypt.ReKeyGen(apub, apri, bpub, 10, 5)
+	if err6 != nil {
+		fmt.Println("ReKeyGen() error:", err6.Error())
+	}
 	fmt.Println(string(plain))
+	fmt.Println("KF:", KF)
 }
--- recrypt/recrypt.go
+++ recrypt/recrypt.go
@@ -2,6 +2,8 @@ package recrypt
 
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
 	"encoding/hex"
 	"fmt"
 	"goUmbral/curve"
@@ -19,6 +21,15 @@ type Capsule struct {
 type Cipher_before_re struct {
 	CipherText []byte
 	Capsule    *Capsule
+}
+
+type KFrag struct {
+	id  *big.Int
+	rk  *big.Int
+	X_A *ecdsa.PublicKey
+	U_1 *ecdsa.PublicKey
+	z_1 *big.Int
+	z_2 *big.Int
 }
 
 func Encapsulate(pubKey *ecdsa.PublicKey) (keyBytes []byte, capsule *Capsule, err error) {
@@ -71,7 +82,7 @@ func Encrypt(pubKey *ecdsa.PublicKey, message string) (cipher *Cipher_before_re,
 }
 
 // Recreate aes key
-func Decapsulate(capsule *Capsule, aPriKey *ecdsa.PrivateKey) (keyBytes []byte, err error) {
+func Decapsulate(aPriKey *ecdsa.PrivateKey, capsule *Capsule) (keyBytes []byte, err error) {
 	point1 := curve.PointScalarAdd(capsule.E, capsule.V)
 	point := curve.PointScalarMul(point1, aPriKey.D)
 	// generate aes key
@@ -83,7 +94,7 @@ func Decapsulate(capsule *Capsule, aPriKey *ecdsa.PrivateKey) (keyBytes []byte, 
 }
 
 func Decrypt(aPriKey *ecdsa.PrivateKey, cipher *Cipher_before_re) (plainText []byte, err error) {
-	keyBytes, err := Decapsulate(cipher.Capsule, aPriKey)
+	keyBytes, err := Decapsulate(aPriKey, cipher.Capsule)
 	if err != nil {
 		return nil, err
 	}
@@ -106,4 +117,76 @@ func CheckCapsule(capsule *Capsule) (err error) {
 		return nil
 	}
 	return fmt.Errorf("%s", "Capsule not match")
+}
+
+func ReKeyGen(aPubKey *ecdsa.PublicKey, aPriKey *ecdsa.PrivateKey, bPubKey *ecdsa.PublicKey, N int, t int) ([]KFrag, error) {
+	if t < 2 {
+		return nil, fmt.Errorf("%s", "t must bigger than 1")
+	}
+	X_A, x_A, err := curve.KeyGen()
+	if err != nil {
+		return nil, err
+	}
+	// get d = H3(X_A,pk_b,pk_b^(X_A))
+	d := utils.HashToCurve(
+		utils.ConcatBytes(
+			curve.PointToBytes(X_A),
+			utils.ConcatBytes(
+				curve.PointToBytes(bPubKey),
+				curve.PointToBytes(curve.PointScalarMul(bPubKey, x_A.D)))))
+	coefficients, err := utils.GetCoefficients(aPriKey.D, d, t)
+	fmt.Println("coefficients:", coefficients)
+	if err != nil {
+		return nil, err
+	}
+	// get D = H6(pk_a,pk_b,pk_b^a)
+	D := utils.HashToCurve(
+		utils.ConcatBytes(
+			curve.PointToBytes(aPubKey),
+			utils.ConcatBytes(
+				curve.PointToBytes(bPubKey),
+				curve.PointToBytes(curve.PointScalarMul(bPubKey, aPriKey.D)))))
+	KF := []KFrag{}
+	for i := 0; i < N; i++ {
+		Y, y, err := curve.KeyGen()
+		if err != nil {
+			return nil, err
+		}
+		id, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		if err != nil {
+			return nil, err
+		}
+		// get s_x = H5(id,D)
+		s_x := utils.HashToCurve(
+			utils.ConcatBytes(
+				id.X.Bytes(),
+				D.Bytes()))
+		rk := utils.GetPolynomialValue(coefficients, s_x)
+		U_1 := curve.BigIntMulBase(rk)
+		// get z_1 = H4(Y,id,pk_a,pk_b,U_1,X_A)
+		z_1 := utils.HashToCurve(
+			utils.ConcatBytes(
+				curve.PointToBytes(Y),
+				utils.ConcatBytes(
+					id.D.Bytes(),
+					utils.ConcatBytes(
+						curve.PointToBytes(aPubKey),
+						utils.ConcatBytes(
+							curve.PointToBytes(bPubKey),
+							utils.ConcatBytes(
+								curve.PointToBytes(U_1),
+								curve.PointToBytes(X_A)))))))
+		// get z_2 = y - a × z_1
+		z_2 := math.BigIntSub(y.D, math.BigIntMul(aPriKey.D, z_1))
+		kFrag := KFrag{
+			id:  id.D,
+			rk:  rk,
+			X_A: X_A,
+			U_1: U_1,
+			z_1: z_1,
+			z_2: z_2,
+		}
+		KF = append(KF, kFrag)
+	}
+	return KF, nil
 }
